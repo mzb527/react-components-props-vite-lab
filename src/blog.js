500af14517d8d4968f13b9c559d55ddaf531592f
mzb527 <mzb527@gmail.com>
2025-04-12 20:28:26 -0400

    Lab Uploaded

---
+++ src/blog.js
@@ -0,0 +1,21 @@
+const blogData = {
+    name: "Mike's Blog",
+    image: "https://via.placeholder.com/150",
+    about: "Welcome to my blog! tHere are no posts. Try again another time.",
+    posts: [
+      {
+        id: 1,
+        title: "Yesterday",
+        date: "April 11, 2025",
+        preview: "Yesterday was very hectic.",
+      },
+      {
+        id: 2,
+        title: "Today",
+        date: "April 12, 2025",
+        preview: "Today was better.",
+      },
+    ],
+  };
+  
+  export default blogData;
